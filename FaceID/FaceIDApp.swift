d0e3c8915e3e689292f2104a796bc4df6c1c8e35
ThibaultGdf <107555179+ThibaultGdf@users.noreply.github.com>
2023-11-20 13:45:18 +0100

    Initial Commit

---
+++ FaceID/FaceIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIDApp.swift
+//  FaceID
+//
+//  Created by Thibault GODEFROY on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FaceIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
